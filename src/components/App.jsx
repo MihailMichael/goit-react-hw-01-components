05483d6fa281bcdd56ff5f4190243dea2112f4aa
Mihail <Maria@MihailMichael>
2023-09-07 19:32:51 +0300

    feat:new code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout/Layout';
+import { TaskSection } from './TaskSection/TaskSection';
+
+import user from './Profile/user.json';
+import { Profile } from './Profile/Profile';
+
+import friends from './Friends/friends.json';
+import { FriendList } from './Friends/FriendList/FriendList';
+
+import data from './Statistics/data.json';
+import { Statistics } from './Statistics/Statistics';
+
+import transactions from './TransactionHistory/transactions.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <h2>1 - Social network profile</h2>
+      <TaskSection>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </TaskSection>
+      <h2>2- Statistics Section</h2>
+      <TaskSection>
+        <Statistics title="Upload stats" stats={data} />
+      </TaskSection>
+      <h2>3 - Friends list</h2>
+      <TaskSection>
+        <FriendList friends={friends} />
+      </TaskSection>
+      <h2>4 - Transaction History</h2>
+      <TaskSection>
+        <TransactionHistory transactions={transactions} />
+      </TaskSection>
+      <GlobalStyle />
+    </Layout>
   );
 };
